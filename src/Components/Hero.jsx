97b4acdb9d6377394193fd87ea35ee3285ac78c5
Suraj Yaduvanshi <pvt.suraj37@gmail.com>
2025-02-24 06:53:31 +0530

    Booking link added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useEffect } from "react";
+import { getCalApi } from "@calcom/embed-react";
 import Navbar from "./Navbar";
 
 const Hero = React.memo(() => {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({ namespace: "30min" });
+      cal("ui", {
+        cssVarsPerTheme: {
+          light: { "cal-brand": "#0C0C0C" },
+          dark: { "cal-brand": "#FCFCFC" },
+        },
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      });
+    })();
+  }, []);
+
   return (
     <div
       data-scroll
@@ -21,11 +37,15 @@ const Hero = React.memo(() => {
         </p>
       </div>
       <div className="xl:hidden">
-        <div className="absolute bottom-4 right-4 sm:bottom-4 sm:right-8 rounded-full text-main bg-accent">
-          <a className="p-2 px-4 pr-5 text-lg flex gap-2 items-center" href="https://bootwebstudio.github.io/Portfolio/" target="_blank">
+        <div
+          data-cal-namespace="30min"
+          data-cal-link="bootwebstudio/30min"
+          data-cal-config='{"layout":"month_view"}'
+          className="absolute bottom-4 right-4 sm:bottom-4 sm:right-8 rounded-full text-main bg-accent">
+          <button className="p-2 px-4 pr-5 text-lg flex gap-2 items-center">
             <i className="ri-phone-fill"></i>
             <span className="">Book a call</span>
-          </a>
+          </button>
         </div>
       </div>
     </div>
